9e33cba61ea2c192ffc2a087afee47a47d00896f
Brent Gauthier <brentsgauthier@gmail.com>
2023-02-12 10:12:18 -0500

    Set up routing to individual pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,46 @@
+import {createBrowserRouter, RouterProvider, Route,} from 'react-router-dom';
+import Create from './pages/Create';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Post from './pages/Post';
+import Register from './pages/Register';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>Home</div>
+  },
+
+  {
+    path: '/create',
+    element: <Create />
+  },
+
+  {
+    path: '/home',
+    element: <Home />
+  },
+
+  {
+    path: '/login',
+    element: <Login />
+  },
+
+  {
+    path: '/post',
+    element: <Post />
+  },
+
+  {
+    path: '/register',
+    element: <Register />
+  },
+]);
+
 function App() {
   return (
     <div>
-        Hello World!
+        <RouterProvider router = {router} />
     </div>
   );
 }
