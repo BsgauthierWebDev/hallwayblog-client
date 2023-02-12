16578f9ed92e531e967b65cfc02b72023b40cca1
Brent Gauthier <brentsgauthier@gmail.com>
2023-02-12 11:22:07 -0500

    Styled NavBar component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <div className = 'navbar'>
+      <div className = 'container'>
+        <div className = 'logo'>logo</div>
+        <div className = 'links'>links</div>
+      </div>
+    </div>
   )
 }
 
