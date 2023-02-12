56814e342e3ef48898f7b12ef6ca9686c6856c27
Brent Gauthier <brentsgauthier@gmail.com>
2023-02-12 14:47:27 -0500

    Built and styled single Post page

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import {Link} from 'react-router-dom';
+import Edit from '../images/edit.png';
+import Delete from '../images/delete.jpg';
 
 const Post = () => {
   return (
-    <div>Post</div>
+    <div className = 'post'>
+      <div className = 'content'>
+        <img src = 'https://sienaconstruction.com/wp-content/uploads/2017/05/test-image.jpg' alt = '' />
+        <div className = 'user'>
+          <img src = 'https://sienaconstruction.com/wp-content/uploads/2017/05/test-image.jpg' alt = '' />
+          <div className = 'info'>
+            <span><i>UserName</i></span>
+            <p>Posted <i>X</i> days ago</p>
+          </div>
+          <div className = 'edit'>
+            <Link to = {`/new?edit = 2`}>
+              <img src = {Edit} alt = 'edit' />
+            </Link>
+            <img src = {Delete} alt = 'delete' />
+          </div>
+        </div>
+      </div>
+      <div className = 'menu'>M</div>
+    </div>
   )
 }
 
