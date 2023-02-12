f784ecd8383ed579ff9eb85bb1c68293637b8e36
Brent Gauthier <brentsgauthier@gmail.com>
2023-02-12 15:14:34 -0500

    Build and style Menu component

---
+++ src/components/Menu.jsx
@@ -0,0 +1,62 @@
+import React from 'react'
+
+const Menu = () => {
+    const posts = [
+        {
+          id: 1,
+          title: 'Post 1',
+          desc: 'This is Post 1',
+          img: 'https://www.freebiefindingmom.com/wp-content/uploads/2020/12/Free-Printable-Number-Bubble-Letters-Bubble-Number-1.jpg'
+        },
+    
+        {
+          id: 2,
+          title: 'Post 2',
+          desc: 'This is Post 2',
+          img: 'https://www.freebiefindingmom.com/wp-content/uploads/2020/12/Free-Printable-Number-Bubble-Letters-Bubble-Number-2.jpg'
+        },
+    
+        {
+          id: 3,
+          title: 'Post 3',
+          desc: 'This is Post 3',
+          img: 'https://www.freebiefindingmom.com/wp-content/uploads/2020/12/Free-Printable-Number-Bubble-Letters-Bubble-Number-3.jpg'
+        },
+    
+        {
+          id: 4,
+          title: 'Post 4',
+          desc: 'This is Post 4',
+          img: 'https://www.freebiefindingmom.com/wp-content/uploads/2020/12/Free-Printable-Number-Bubble-Letters-Bubble-Number-4.jpg'
+        },
+    
+        {
+          id: 5,
+          title: 'Post 5',
+          desc: 'This is Post 5',
+          img: 'https://www.freebiefindingmom.com/wp-content/uploads/2020/12/Free-Printable-Number-Bubble-Letters-Bubble-Number-5.jpg'
+        },
+    
+        {
+          id: 6,
+          title: 'Post 6',
+          desc: 'This is Post 6',
+          img: 'https://www.freebiefindingmom.com/wp-content/uploads/2020/12/Free-Printable-Number-Bubble-Letters-Bubble-Number-6.jpg'
+        }
+      ]
+
+  return (
+    <div className = 'menu'>
+        <h1>Suggested Posts</h1>
+        {posts.map((post) => (
+            <div className = 'post' key = {post.id}>
+                <img src = {post.img} alt = '' />
+                <h2>{post.title}</h2>
+                <button>Read More</button>
+            </div>
+        ))}
+    </div>
+  )
+}
+
+export default Menu
